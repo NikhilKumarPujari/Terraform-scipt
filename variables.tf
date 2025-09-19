27dadb2f4c9c18ae0c4d25cbd07c4badea12a4f6
Nikhil Kumar <144585007+NikhilKumarPujari@users.noreply.github.com>
2025-09-19 15:06:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,42 +1,38 @@
-# Common variables
+variable "region" {
+  default = "ap-south-1"
+}
+
 variable "env" {
   description = "Environment name (dev, stage, prod)"
-  type        = string
 }
 
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
-}
-
-# EC2 variables
-variable "ami_id" {
-  description = "AMI ID for EC2"
-  type        = string
-}
-
+# EC2
+variable "ami_id" {}
 variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.micro"
+  default = "t2.micro"
 }
+variable "key_name" {}
+variable "subnet_id" {}
 
-variable "key_name" {
-  description = "SSH key pair name"
-  type        = string
-}
-
-# VPC variables
+# VPC
 variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
-# S3 variables
+# S3
 variable "enable_s3" {
-  description = "Whether to create an S3 bucket (true/false)"
-  type        = bool
-  default     = false
+  default = false
+}
+variable "bucket_name" {
+  default = ""
+}
+
+# EBS
+variable "ebs_volume_size" {
+  default = 8
+}
+
+# IAM
+variable "iam_user" {
+  default = "demo-user"
 }
